cef19ff6c0856bed8af1d779e386fb7d2a5acf94
Pratik Koli <pkoli4470@gmail.com>
2024-02-21 13:08:18 +0530

    Book entities added

---
+++ QueryDsl/src/main/java/com/app/entities/Book.java
@@ -0,0 +1,16 @@
+package com.app.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String name;
+    String isbn;
+
+}
